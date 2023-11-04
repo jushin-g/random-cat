3e8c5e739a80cc5d1598017a46a5ed8db2077fe3
jushin-g <momonobu1048dong@au.com>
2023-11-04 12:17:41 +0900

    refactor: ボタンの変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }: Props) => {
   return (
     <div className={styles.page}>
       <button onClick={handleClick} className={styles.button}>
-        他のにゃんこも見る
+        One More Cat!
       </button>
       <div className={styles.frame}>
         {loading || <img src={imageUrl} className={styles.img} />}
